9b8481a86cf85df986b8aa5f6ba85041d30b7275
KChi <59391215+KagChi@users.noreply.github.com>
2022-02-06 10:46:06 +0700

    chore: remove console.log

--- src/__tests__/getPlaylist.test.ts
+++ src/__tests__/getPlaylist.test.ts
@@ -12,7 +12,6 @@ test("Get apple music video ", (done) => {
     });
     
     manager.search("https://music.apple.com/us/playlist/rick-astley-essentials/pl.504a9420747e43ec93e4faa999a8bef9").then(x => {
-        console.log(x)
         expect(x.loadType).toBe("PLAYLIST_LOADED");
         done();
     }).catch(e => expect(e).toBeUndefined());
