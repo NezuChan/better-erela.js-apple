2497a60fb7fee8bb6b718b333c2366f2eaa6a20d
KChi <59391215+KagChi@users.noreply.github.com>
2022-02-06 10:48:30 +0700

    fix: missing cache track

--- src/Manager/PlaylistManager.ts
+++ src/Manager/PlaylistManager.ts
@@ -18,6 +18,7 @@ export class PlaylistManager extends BaseManager {
             const data = await response.json() as MusicVideoAPIREsponse;
             if (data.errors && !data.data) return this.buildSearch("NO_MATCHES", undefined, "Could not find any suitable track(s), unexpected apple music response", undefined);
             const fileredData = data.data?.filter(x => x.type === "playlists")[0]!.relationships.tracks.data.filter(x => x.type === "songs")!;
+            if (this.resolver.plugin.options.cacheTrack) this.cache.set(id, { tracks: fileredData.map(x => ({ name: x.attributes.name, uri: x.attributes.url, artist: x.attributes.artistName, duration: x.attributes.durationInMillis * 1000 })), name: data.data?.filter(x => x.type === "playlists")[0]!.attributes.name });
             return this.buildSearch("PLAYLIST_LOADED", this.resolver.plugin.options.convertUnresolved ? await this.autoResolveTrack(fileredData.map(x => TrackUtils.buildUnresolved(this.buildUnresolved({ name: x.attributes.name, uri: x.attributes.url, artist: x.attributes.artistName, duration: x.attributes.durationInMillis * 1000 }), requester))) : fileredData.map(x => TrackUtils.buildUnresolved(this.buildUnresolved({ name: x.attributes.name, uri: x.attributes.url, artist: x.attributes.artistName, duration: x.attributes.durationInMillis * 1000 }), requester)), undefined, data.data![0].attributes.name);
         } catch(e) {
             return this.buildSearch("NO_MATCHES", undefined, "Could not find any suitable track(s), unexpected apple music response", undefined);
